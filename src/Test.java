a4bf5d4fbdd992775cd502fcebd0fa4e77fb4a29
Soham Dasgupta <sohamd@hotmail.com>
2018-01-30 10:42:15 -0600

    display array of Greeters

--- src/Test.java
+++ src/Test.java
@@ -6,6 +6,13 @@ public class Test {
 
 		System.out.println(g.toString());
 
+		Greeter[] gs = new Greeter[5];
+
+		for (int x = 0; x < 5; x++)
+			gs[x] = new Greeter();
+
+		for (int y = 0; y < 5; y++)
+			System.out.println(gs[y].toString());
 	}
 
 }
